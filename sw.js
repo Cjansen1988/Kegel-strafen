c5afdc8aa37f1eaf2d1fb180d4ec2898162f7f0d
Cjansen1988 <cjansen@mail.de>
2026-05-07 00:33:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,4 @@
-const CACHE_NAME = 'kegel-strafen-v1';
-const FILES = [
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
-});
+const CACHE = 'kegel-spiele-v1';
+const FILES = ['./index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
